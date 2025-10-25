0596085ae026bbddec0d9e5e8ab8a7cb2178deae
RIRony9898 <rirony59@gmail.com>
2025-10-25 17:47:34 +0600

    carousel update

--- src/Components/HomePage/Banner.jsx
+++ src/Components/HomePage/Banner.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+import img1 from "../../assets/1.jpg";
+import img2 from "../../assets/2.jpg";
+import img3 from "../../assets/3.jpg";
+import img4 from "../../assets/4.jpg";
 
 const Banner = () => {
+  const carouselRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  console.log(currentSlide)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => {
+        const next = (prev + 1) % 4;
+        if (carouselRef.current) {
+          const slideWidth = carouselRef.current.offsetWidth;
+          carouselRef.current.scrollLeft = next * slideWidth;
+        }
+        return next;
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-      <div className="carousel w-full">
+      <div ref={carouselRef} className="carousel w-full max-h-96">
         <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
-          />
+          <img src={img1} className="w-full" alt="Slide 1" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -19,10 +39,7 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="w-full"
-          />
+          <img src={img2} className="w-full" alt="Slide 2" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -33,10 +50,7 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="w-full"
-          />
+          <img src={img3} className="w-full" alt="Slide 3" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -47,10 +61,7 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="w-full"
-          />
+          <img src={img4} className="w-full" alt="Slide 4" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide3" className="btn btn-circle">
               ❮
