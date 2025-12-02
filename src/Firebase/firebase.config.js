2d1f915cb3db762fbccafcb0c8838896ea67ed68
RIRony9898 <rirony59@gmail.com>
2025-12-02 18:33:45 +0600

    Update Firebase configuration to use environment variables

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA06ZKGzuPishzgErkuw6iRBJYQpZdSf_Q",
-  authDomain: "best-store-project-f9e93.firebaseapp.com",
-  projectId: "best-store-project-f9e93",
-  storageBucket: "best-store-project-f9e93.firebasestorage.app",
-  messagingSenderId: "659679116401",
-  appId: "1:659679116401:web:579a540fea8491524a2fd0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
